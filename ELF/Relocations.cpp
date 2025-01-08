5d457812dd5b666660a83dc3ec6e27fe0382932e
Svetla Syrimis <svetla@wire.network>
2025-01-08 03:46:38 +0000

    updated tools/lld *

--- ELF/Relocations.cpp
+++ ELF/Relocations.cpp
@@ -1528,7 +1528,7 @@ void ThunkCreator::mergeThunks(ArrayRef<OutputSection *> outputSections) {
         // those inserted in previous passes. Extract the Thunks created this
         // pass and order them in ascending outSecOff.
         std::vector<ThunkSection *> newThunks;
-        for (const std::pair<ThunkSection *, uint32_t> ts : isd->thunkSections)
+        for (const std::pair<ThunkSection *, uint32_t> &ts : isd->thunkSections)
           if (ts.second == pass)
             newThunks.push_back(ts.first);
         llvm::stable_sort(newThunks,
